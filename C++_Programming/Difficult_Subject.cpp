5413c5d3fc6f5f7f163911be3331defa0753e2a8
Inamuddin Ahmed <91588532+InamuddinAhmed@users.noreply.github.com>
2021-10-12 18:00:51 +0530

    Difficult Subject Program

---
+++ C++_Programming/Difficult_Subject.cpp
@@ -0,0 +1,64 @@
+/*
+Difficult Subject
+
+The principal of the School has asked Sankey to create a tool which will analyze students score and identify subjects that students find difficult to crack. Below are scores of students for given subjects. Write a program that will take the below data and identify the subject that students find difficult to crack.
+Sample Input:
+
+Students                    English(60)     History(40)     Computers(50)   Mathematics(100)    Science(90)    Economics(80)
+
+Subject's-(Max.Marks)           100             100             130             150                 100              100
+
+Meera                           80              70              80              90                  80               60
+
+Subodh Kunal                    90              70              60              90                  70               40
+
+Soni                            60              60              65              80                  80               70
+
+Richu                           50              90              62              80                  85               80
+
+Irene                           40              60              64              70                  65               95
+
+Vijay                           80              80              35              65                  50               85
+
+The data in the above table is input to your program. Define your own way to take input in your program, write comments accordingly.
+
+Sample Output:
+
+Print name of a subject which is difficult to crack students.
+
+Explanation:
+
+No explanation is available as it is an analytical question. You need to come up with your own solution to get the required output,
+*/
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int n,i,j;
+    cin>>n;
+    int arr[n][n];
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+    int a=0;
+    int b=0;
+    int max=0;
+    for (i=0; i <n; i++)
+    {
+        for (j=0; j < n; j++)
+        {
+            if(arr[i][j]>max)
+            {
+                max=arr[i][j];
+                a=i;
+                b=j;
+            }
+        }
+    }
+    return 0;
+}
